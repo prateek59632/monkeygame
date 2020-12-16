9bb53bf53f7d30630ab6c672b324fa82951d42cc
prateek59632 <75611941+prateek59632@users.noreply.github.com>
2020-12-16 01:47:20 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+
+ 
+}
+
+
+
+function setup() {
+ monkey=createSprite(80,315,20,20) 
+monkey.addAnimation("moving",monkey_running)
+  monkey.scale=0.1
+  ground=createSprite(400,350,900,10);
+  
+  ground.velocityX=-4;
+  
+ 
+  console.log(ground.x)
+}
+
+
+function draw() {
+background(225)
+  drawSprites();
+  ground.lifetime=800
+  if(ground.x<0){
+ ground.x=ground.widht/2;
+  }
+  if(frameCount%60===0){
+       bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+  
+     }
+ 
+    if(keyDown("space")){
+     monkey.velocityY=-12
+  }  
+  monkey.velocityY=monkey.velocityY+0.8
+monkey.collide(ground);
+}
+ 
+
+
+
+
+
+
